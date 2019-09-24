17372b876274a1187632bff548b893039816880b
DiegoCanalesR <diego.canales.r@ing.uchile.cl>
2019-09-24 20:31:51 -0300

    layers y modelo

--- modelo.py
+++ modelo.py
@@ -1,66 +1,69 @@
 # -*- coding: utf-8 -*-
 
 import tensorflow as tf
-from layers import conv_layer
-from layers import fc_layer
-from layers import fusion_layer
-from layers import output_layer
+
+image_size = 224
+batch_size = 1
 
 
+##Capas a utilizar
+#Capa convolucional
+def conv_layer(layer_name, tensor, shape, stride):
+    weights = tf.get_variable("weights", shape, initializer = tf.contrib.layers.xavier_initializer_conv2d())
+    biases = tf.get_variable("biases", [shape[3]], initializer=tf.constant_initializer(0.05))
+    tf.summary.histogram(layer_name + "/weights", weights)
+    tf.summary.histogram(layer_name + "/biases", biases)
+    conv = tf.nn.conv2d(tensor, weights, stride, padding='SAME')
+    return tf.nn.relu(conv + biases)
+
+#Capa fully-connected
+def fc_layer(layer_name, tensor, shape):
+    weights = tf.get_variable("weights", shape, initializer = tf.contrib.layers.xavier_initializer())
+    biases = tf.get_variable("biases", [shape[1]], initializer=tf.constant_initializer(0.0))
+    tf.summary.histogram(layer_name + "/weights", weights)
+    tf.summary.histogram(layer_name + "/biases", biases)
+    mult_out = tf.matmul(tensor, weights)
+    return tf.nn.relu(mult_out+biases)
+
+#Capa de fusion de features
+def fusion_layer(midlvl_features, global_features, shape, stride):
+    midlvlft_shape = midlvl_features.get_shape().as_list()
+    new_shape = [batch_size, midlvlft_shape[1]*midlvlft_shape[2], 256]
+    midlvlft_reshaped = tf.reshape(midlvlft_shape, new_shape)
+    fusion_lvl = []
+    for j in range(midlvlft_reshaped[0]):
+        for i in range(midlvlft_reshaped[1]):
+             see_mid = midlvlft_reshaped[j, i, :]
+             see_mid_shape = see_mid.get_shape().as_list()
+             see_mid = tf.reshape(see_mid, [1, see_mid_shape[0]])
+             global_features_shape = global_features[j, :].get_shape().as_list()
+             see_global = tf.reshape(global_features[j, :], [1, global_features_shape[0]])
+             fusion = tf.concat([see_mid, see_global], 1)
+             fusion_lvl.append(fusion)
+    fusion_lvl = tf.stack(fusion_lvl, 1)
+    fusion_shape = [batch_size, 28, 28, 512]
+    fusion_lvl = tf.reshape(fusion_lvl, fusion_shape)
+    return conv_layer('Fusion', fusion_lvl, shape, stride)
+
+#Capa de salida
+def output_layer(tensor, shape, stride):
+    weights = tf.get_variable("weights", shape, initializer = tf.contrib.layers.xavier_initializer_conv2d())
+    biases = tf.get_variable("biases", [shape[3]], initializer=tf.constant_initializer(0.05))
+    conv = tf.nn.conv2d(tensor, weights, stride, padding='SAME')
+    output_data = tf.nn.sigmoid(tf.nn.bias_add(conv, biases))
+    return output_data
+
+
+#Clase para el modelo
 class modelo():
     
     #Inicializacion
     def __init__(self):
-        self.inputs = tf.placeholder(shape=[config.BATCH_SIZE, config.IMAGE_SIZE, config.IMAGE_SIZE, 1], dtype=tf.float32)
-        self.labels = tf.placeholder(shape=[config.BATCH_SIZE, config.IMAGE_SIZE, config.IMAGE_SIZE, 2], dtype=tf.float32)
+        self.inputs = tf.placeholder(shape=[batch_size, image_size, image_size, 1], dtype=tf.float32)
+        self.labels = tf.placeholder(shape=[batch_size, image_size, image_size, 2], dtype=tf.float32)
         self.loss = None
         self.output = None
         
-    #Capa convolucional
-    def conv_layer(layer_name, tensor, shape, stride):
-        weights = tf.get_variable("weights", shape, initializer = tf.contrib.layers.xavier_initializer_conv2d())
-        biases = tf.get_variable("biases", [shape[3]], initializer=tf.constant_initializer(0.05))
-        tf.summary.histogram(layer_name + "/weights", weights)
-        tf.summary.histogram(layer_name + "/biases", biases)
-        conv = tf.nn.conv2d(tensor, weights, stride, padding='SAME')
-        return tf.nn.relu(conv + biases)
-    
-    #Capa fully-connected
-    def fc_layer(layer_name, tensor, shape):
-        weights = tf.get_variable("weights", shape, initializer = tf.contrib.layers.xavier_initializer())
-        biases = tf.get_variable("biases", [shape[1]], initializer=tf.constant_initializer(0.0))
-        tf.summary.histogram(layer_name + "/weights", weights)
-        tf.summary.histogram(layer_name + "/biases", biases)
-        mult_out = tf.matmul(tensor, weights)
-        return tf.nn.relu(mult_out+biases)
-    
-    #Capa de fusion de features
-    def fusion_layer(midlvl_features, global_features, shape, stride):
-        midlvlft_shape = midlvl_features.get_shape().as_list()
-        new_shape = [config.batch_size, midlvlft_shape[1]*midlvlft_shape[2], 256]
-        midlvlft_reshaped = tf.reshape(midlvlft_shape, new_shape)
-        fusion_lvl = []
-        for j in range(midlvlft_reshaped[0]):
-            for i in range(midlvlft_reshaped[1]):
-                 see_mid = midlvlft_reshaped[j, i, :]
-                 see_mid_shape = see_mid.get_shape().as_list()
-                 see_mid = tf.reshape(see_mid, [1, see_mid_shape[0]])
-                 global_features_shape = global_features[j, :].get_shape().as_list()
-                 see_global = tf.reshape(global_features[j, :], [1, global_features_shape[0]])
-                 fusion = tf.concat([see_mid, see_global], 1)
-                 fusion_lvl.append(fusion)
-        fusion_lvl = tf.stack(fusion_lvl, 1)
-        fusion_shape = [config.batch_size, 28, 28, 512]
-        fusion_lvl = tf.reshape(fusion_lvl, fusion_shape)
-        return conv_layer('Fusion', fusion_lvl, shape, stride)
-    
-    #Capa de salida
-    def output_layer(tensor, shape, stride):
-        weights = tf.get_variable("weights", shape, initializer = tf.contrib.layers.xavier_initializer_conv2d())
-        biases = tf.get_variable("biases", [shape[3]], initializer=tf.constant_initializer(0.05))
-        conv = tf.nn.conv2d(tensor, weights, stride, padding='SAME')
-        output_data = tf.nn.sigmoid(tf.nn.bias_add(conv, biases))
-        return output_data
     
     #Construccion del modelo
     def build_model(self):
@@ -83,7 +86,7 @@ class modelo():
         globalft = conv_layer('globalft_conv3', globalft, shape=[3, 3, 512, 512], stride=[1, 2, 2, 1])
         globalft = conv_layer('globalft_conv4', globalft, shape=[3, 3, 512, 512], stride=[1, 1, 1, 1])
         #capas fc para global features
-        global_flat = tf.reshape(globalft, [config.BATCH_SIZE, -1])
+        global_flat = tf.reshape(globalft, [batch_size, -1])
         dim = global_flat.get_shape()[1].value
         globalft = fc_layer('global_fc1', global_flat, shape=[dim, 1024])
         globalft = fc_layer('global_fc2', globalft, shape=[1024, 512])
@@ -105,8 +108,6 @@ class modelo():
         output = tf.image.resize_images(output, [224, 224], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
         self.output = tf.image.resize_images(output, [224, 224], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
         self.loss = tf.reduce_mean(tf.squared_difference(self.labels, self.output))
-
-
 
 
 
