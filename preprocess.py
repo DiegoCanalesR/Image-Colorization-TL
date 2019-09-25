7c53127da4ecc4913ab1ed630332b0fc9dc4bcdd
DiegoCanalesR <diego.canales.r@ing.uchile.cl>
2019-09-24 21:52:13 -0300

    +batches train

--- preprocess.py
+++ preprocess.py
@@ -16,43 +16,37 @@ def tiny_imagenet_txt(set_type, img_type):
     with open(txt, 'w') as f:
         for img in imgs:
             f.write("%s\n" % img)
-    
-def gray(imgs):
-    gray_imgs = []
-    for img in imgs:
-        img = cv2.imread(img)
-        grayimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        gray_imgs.append(grayimg)
-    return gray_imgs
 
-
-def get_ab(imgs):
+def get_gray_and_ab(imgs):
     a = []
     b = []
+    gray = []
     for img in imgs:
         img = cv2.imread(img)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+        gray.append(img[:,:,0])
         a.append(img[:,:,1])
         b.append(img[:,:,2])
-    return a,b
+    return gray,a,b
 
-def train_set_tiny_imagenet(cant):
+def train_batch_tiny_imagenet(batch_size):
     path = os.getcwd()  + '\\tiny-imagenet-200\\train'#+ set_type
     txt = path + '\\train_paths.txt'
     lines = open(txt).read().splitlines()
     shuffle(lines)
-    imgs = lines[:cant]
+    imgs = lines[:batch_size]
     return imgs
         
-#tiny_imagenet_txt('train', '.JPEG')
-#tiny_imagenet_txt('test', '.JPEG')
-#tiny_imagenet_txt('val', '.JPEG')  
+tiny_imagenet_txt('train', '.JPEG')
+tiny_imagenet_txt('test', '.JPEG')
+tiny_imagenet_txt('val', '.JPEG')  
   
-#imags = train_set_tiny_imagenet(10)  
+imags = train_batch_tiny_imagenet(2)  
 
-#gry = gray(imags)   
-#plt.imshow(gry[0], cmap = 'gray'); plt.axis('off'); plt.show()
-
-#a,b = get_ab(imags)
-#plt.imshow(a[0]); plt.axis('off'); plt.show()
-#plt.imshow(b[0]); plt.axis('off'); plt.show()
+g,a,b = get_gray_and_ab(imags)
+for gs in g:
+    plt.imshow(gs, cmap ='gray'); plt.axis('off'); plt.show()
+for aa in a:
+    plt.imshow(aa); plt.axis('off'); plt.show()
+for bb in b:
+    plt.imshow(bb); plt.axis('off'); plt.show()
