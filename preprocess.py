5e7156f6dd4f2be6432ca0d7d399c040edac5e6c
DiegoCanalesR <diego.canales.r@ing.uchile.cl>
2019-09-24 23:18:01 -0300

    correcion batches

--- preprocess.py
+++ preprocess.py
@@ -6,7 +6,7 @@ from random import shuffle
 import matplotlib.pyplot as plt
 
 def tiny_imagenet_txt(set_type, img_type):
-    path = os.getcwd()  + '\\tiny-imagenet-200\\'+ set_type
+    path = os.getcwd() + '\\tiny-imagenet-200\\'+ set_type
     imgs = []
     for (root,dirs,files) in os.walk(path, topdown=True): 
         for file in files:
@@ -21,13 +21,15 @@ def get_gray_and_ab(imgs):
     a = []
     b = []
     gray = []
+    ab = []
     for img in imgs:
         img = cv2.imread(img)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
         gray.append(img[:,:,0])
         a.append(img[:,:,1])
         b.append(img[:,:,2])
-    return gray,a,b
+        ab.append(img[:,:,1:])
+    return gray,a,b,ab
 
 def train_batch_tiny_imagenet(batch_size):
     path = os.getcwd()  + '\\tiny-imagenet-200\\train'#+ set_type
@@ -35,7 +37,8 @@ def train_batch_tiny_imagenet(batch_size):
     lines = open(txt).read().splitlines()
     shuffle(lines)
     imgs = lines[:batch_size]
-    return imgs
+    gray, a, b, ab = get_gray_and_ab(imgs)
+    return imgs, gray, ab
         
 tiny_imagenet_txt('train', '.JPEG')
 tiny_imagenet_txt('test', '.JPEG')
@@ -43,10 +46,10 @@ tiny_imagenet_txt('val', '.JPEG')
   
 imags = train_batch_tiny_imagenet(2)  
 
-g,a,b = get_gray_and_ab(imags)
-for gs in g:
-    plt.imshow(gs, cmap ='gray'); plt.axis('off'); plt.show()
-for aa in a:
-    plt.imshow(aa); plt.axis('off'); plt.show()
-for bb in b:
-    plt.imshow(bb); plt.axis('off'); plt.show()
+#g,a,b,_ = get_gray_and_ab(imags)
+#for gg in g:
+#    plt.imshow(gs, cmap ='gray'); plt.axis('off'); plt.show()
+#for aa in a:
+#    plt.imshow(aa); plt.axis('off'); plt.show()
+#for bb in b:
+#    plt.imshow(bb); plt.axis('off'); plt.show()
