689ffe86fd2ffe63567c40d9af807e4b07f6b36e
DiegoCanalesR <diego.canales.r@ing.uchile.cl>
2019-09-25 18:29:56 -0300

    img resize

--- preprocess.py
+++ preprocess.py
@@ -19,6 +19,12 @@ def tiny_imagenet_txt(set_type, img_type):
         for img in imgs:
             f.write("%s\n" % img)
 
+def resize(img, size):
+    resized = cv2.resize(img, dsize=(size, size), interpolation=cv2.INTER_CUBIC)
+    resized = np.expand_dims(resized,2)
+    return resized
+    
+
 def get_gray_and_ab(imgs):
     a = []
     b = []
@@ -27,10 +33,14 @@ def get_gray_and_ab(imgs):
     for img in imgs:
         img = cv2.imread(img)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-        gray.append(np.resize(img[:,:,0],[224,224,1]))
-        a.append(img[:,:,1])
+        gray.append(resize(img[:,:,0],224))
+        #gray.append(img[:,:,0])
+        a.append(np.expand_dims(img[:,:,1],2))
         b.append(img[:,:,2])
-        ab.append(np.resize(img[:,:,1:],[224,224,2]))
+        a_resized = resize(img[:,:,1],224)
+        b_resized = resize(img[:,:,2],224)
+        ##ab.append(img[:,:,1:])
+        ab.append(np.concatenate((a_resized, b_resized),2))
     return gray,a,b,ab
 
 def batch_tiny_imagenet(set_type, batch_size):
@@ -42,10 +52,10 @@ def batch_tiny_imagenet(set_type, batch_size):
     gray, a, b, ab = get_gray_and_ab(imgs)
     return imgs, gray, ab
 
-def join_image(X, Y_pred, batch_size, imgs):
+def join_image(X, Y, batch_size, imgs):
     for batch in range(batch_size):
         image = np.concatenate((X,Y),2)
-        image = cv2.cvtColor(result, cv2.COLOR_LAB2BGR)
+        image = cv2.cvtColor(image, cv2.COLOR_LAB2BGR)
         name = imgs[batch][-14:]
         save_path = os.getcwd() + '\\tiny-imagenet-200\\img_model1'+name
   
